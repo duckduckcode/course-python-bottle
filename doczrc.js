29f5d363c88e2e49c17bee282047a925eca7e6c6
tarik <tarik.nzl@gmail.com>
2021-01-24 22:00:04 +1300

    updates

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,12 @@
 export default {
   typescript: true,
   public: "/public",
+  menu: [
+    'Installation',
+    'Starting a Website',
+    'Create a Home Page',
+    'Display dynamic content',
+    'Add support for CSS and Images',
+    'Guess My Age',
+  ]
 };
