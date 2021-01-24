d911440b0fcb635772d45ba75c2ec814d0f46c93
tarik <tarik.nzl@gmail.com>
2021-01-25 08:41:26 +1300

    use vscode syntax highlight

--- doczrc.js
+++ doczrc.js
@@ -1,13 +1,20 @@
 export default {
-  title: 'Websites with Python and Bottle',
+  title: "Websites with Python and Bottle",
   typescript: true,
   public: "/public",
   menu: [
-    'Installation',
-    'Starting a Website',
-    'Create a Home Page',
-    'Display dynamic content',
-    'Add support for CSS and Images',
-    'Guess My Age',
-  ]
+    "Installation",
+    "Starting a Website",
+    "Create a Home Page",
+    "Display dynamic content",
+    "Add support for CSS and Images",
+    "Guess My Age",
+  ],
+  gatsbyRemarkPlugins: [
+    {
+      resolve: "gatsby-remark-vscode",
+      // OPTIONAL
+      options: {},
+    },
+  ],
 };
